4e01cf6b29abb62b5b672ed2f4263cb17701958a
Duane Bradbury <dibely@hotmail.com>
2017-03-30 14:07:35 +0100

    Abstracted the audio spectrum analysis functionality into it's own class. Added support for determining the dominant frequency band and pitch value

---
+++ Assets/Scripts/AudioSpectrumAnalyser.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+
+//
+// Summary:
+//     ///
+//     An AudioSource spectrum anlyser that tracks key data about the AudioSource during playback 
+//
+//     References:
+//     http://answers.unity3d.com/questions/157940/getoutputdata-and-getspectrumdata-they-represent-t.html
+//     ///
+public class AudioSpectrumAnalyser  {
+    private const float MINIMUM_AMPLITUDE_FOR_PITCH = 0.02f;
+
+    // Constant values for sample sizes that must be a power of 2.
+    public enum AudioSampleSize : uint {
+        VerySmall = 64,
+        Small = 128,
+        Medium = 256,
+        Large = 512,
+        VeryLarge = 1024
+    }
+
+    private readonly AudioSource audioSource;
+    private readonly AudioSampleSize sampleSize;
+    private readonly FFTWindow fftWindow;
+
+    private float[] spectrumData;
+    public float[] audioSpectrumData { get { return spectrumData; } }
+
+    private float pitchValue = 0.0f;
+    public float pitch { get { return pitchValue; } }
+
+    private uint maxAmplitudeBandIndex = 0;
+    public uint dominantFrequencyBandIndex { get { return maxAmplitudeBandIndex; }  }
+
+    //
+    // Summary:
+    //     ///
+    //     Create a new audio spectrum analyser
+    //     ///
+    //
+    // Parameters:
+    //   audioSource:
+    //     AudioSource to analyse.
+    //   audioSampleSize:
+    //     One of the sample size constants that represent different powers of 2 to ensure valid input.  
+    //   audioFFTWindow:
+    //     The FFT to apply when accessing the spectrum data from the AudioSource.
+    public AudioSpectrumAnalyser(AudioSource audioSource, AudioSampleSize audioSampleSize, FFTWindow audioFFTWindow) {
+        this.audioSource = audioSource;
+        fftWindow = audioFFTWindow;
+
+        sampleSize = audioSampleSize;
+        spectrumData = new float[(uint)sampleSize];
+    }
+
+    //
+    // Summary:
+    //     ///
+    //     Updates the audio source spectrum data and any additional processing
+    //     ///
+    public void UpdateSpectrumData() {
+        if(audioSource == null) {
+            Debug.Log("Invalid audio source specified");
+            return;
+        }
+
+        audioSource.GetSpectrumData(spectrumData, 0, fftWindow);
+
+        pitchValue = DeterminePitch();
+    }
+
+    //
+    // Summary:
+    //     ///
+    //     Determines the dominant frequency band and pitch value from the sampled audio spectrum
+    //     ///
+    private float DeterminePitch() {
+        float maxAmplitude = 0.0f;
+        for (uint index = 0; index < (uint)sampleSize; index++) {
+            if (spectrumData[index] > maxAmplitude && spectrumData[index] > MINIMUM_AMPLITUDE_FOR_PITCH) {
+                maxAmplitude = spectrumData[index];
+                maxAmplitudeBandIndex = index;
+            }
+        }
+
+        float amplitudeBandIndex = maxAmplitudeBandIndex;
+        if (maxAmplitudeBandIndex > 0 && (maxAmplitudeBandIndex < (uint)sampleSize - 1)) { 
+            float previousBandIndexAmplitude = spectrumData[maxAmplitudeBandIndex - 1] / maxAmplitude;
+            float nextBandIndexAmplitude = spectrumData[maxAmplitudeBandIndex + 1] / maxAmplitude;
+            amplitudeBandIndex += 0.5f * (previousBandIndexAmplitude * previousBandIndexAmplitude - nextBandIndexAmplitude * nextBandIndexAmplitude);
+        }
+
+        return amplitudeBandIndex * ((AudioSampleRate() * 0.5f) / (uint)sampleSize);
+    }
+
+    //
+    // Summary:
+    //     ///
+    //     Wrapper method for the system sample rate
+    //     ///
+    private float AudioSampleRate() {
+        return AudioSettings.outputSampleRate;
+    }
+}
--- Assets/Scripts/AudioToTextureProcessor.cs
+++ Assets/Scripts/AudioToTextureProcessor.cs
@@ -1,24 +1,29 @@
 ﻿using UnityEngine;
 
+//
+// Summary:
+//     ///
+//     Provides functionality for converting a playing AudioSource spectrum into texture data for display via a renderer
+//     ///
 [RequireComponent (typeof(MeshRenderer), typeof(AudioSource))]
 public class AudioToTextureProcessor : MonoBehaviour {
-    private static int AUDIO_SAMPLE_SIZE = 256;
-
     [Tooltip("The hieght of the texture to show a histogram of the audio specturm data. 1 means no histogram data.")]
     [Range(1, 100)]
     public int audioTextureHeight = 50;
+    private int audioTextureWidth;
 
-    private AudioSource audioSource;
+    private AudioSpectrumAnalyser audioSpectrumAnalyser;
     private Material audioMaterial;
-    private float[] audioSampleData;
     private Texture2D audioTexture;
 
 
 	// Use this for initialization
 	void Start () {
-        audioSource = gameObject.GetComponent<AudioSource>();
-        audioSampleData = new float[AUDIO_SAMPLE_SIZE];
-        audioTexture = new Texture2D(AUDIO_SAMPLE_SIZE, audioTextureHeight, TextureFormat.RGBA32, false);
+        AudioSource audioSource = gameObject.GetComponent<AudioSource>();
+        audioSpectrumAnalyser = new AudioSpectrumAnalyser(audioSource, AudioSpectrumAnalyser.AudioSampleSize.Medium, FFTWindow.Rectangular);
+
+        audioTextureWidth = (int)AudioSpectrumAnalyser.AudioSampleSize.Medium;
+        audioTexture = new Texture2D(audioTextureWidth, audioTextureHeight, TextureFormat.RGBA32, false);
 
         MeshRenderer meshRenderer = GetComponent<MeshRenderer>();
         audioMaterial = meshRenderer.material;
@@ -27,22 +32,37 @@ public class AudioToTextureProcessor : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        audioSource.GetSpectrumData(audioSampleData, 0, FFTWindow.Triangle);
-        //Debug.Log("Begin Sample Data");
-
         ShiftAudioHistogramTextureData();
 
-        for (int sampleIndex = 0; sampleIndex < AUDIO_SAMPLE_SIZE; sampleIndex++) {
-            //Debug.Log("Sample Data: " + audioSampleData[sampleIndex]);
-            Color pixel = new Color((audioSampleData[sampleIndex] * 255.0f), 0.0f, 0.0f, 0.0f);
-            audioTexture.SetPixel(sampleIndex, 0, pixel);
+        audioSpectrumAnalyser.UpdateSpectrumData();
+
+        float[] audioSpectrumData = audioSpectrumAnalyser.audioSpectrumData;
+
+        //Debug.Log("Begin Sample Data");
+        for (int index = 0; index < audioTextureWidth; index++) {
+            //Debug.Log("Sample Data: " + audioSpectrumData[index]);
+
+            float red = 0.0f, blue = 0.0f, green = 0.0f;
+            if(index == audioSpectrumAnalyser.dominantFrequencyBandIndex) {
+                green = (audioSpectrumData[index] * 255.0f);
+            }
+            else {
+                red = (audioSpectrumData[index] * 255.0f);
+            }
+
+            Color pixel = new Color(red, green, blue, 0.0f);
+            audioTexture.SetPixel(index, 0, pixel);
         }
         //Debug.Log("End Sample Data");
 
         audioTexture.Apply();
     }
 
-    // Shift the pixel data leaving from the first row onwards.
+    //
+    // Summary:
+    //     ///
+    //     Shifts the pixel data within the audio texture up by one row.
+    //     ///
     void ShiftAudioHistogramTextureData() {
         if (audioTexture.height > 1) {
             Color[] pixelData = audioTexture.GetPixels(0, 0, audioTexture.width, audioTexture.height - 1);
