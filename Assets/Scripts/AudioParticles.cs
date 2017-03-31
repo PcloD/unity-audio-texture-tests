8325dfd20408d87174251868f748716053d05dda
Duane Bradbury <dibely@hotmail.com>
2017-03-31 10:43:55 +0100

    Added basic particle system control script using the audio spectrum

---
+++ Assets/Scripts/AudioParticles.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(ParticleSystem), typeof(AudioSource))]
+public class AudioParticles : MonoBehaviour {
+    private AudioSpectrumAnalyser audioSpectrumAnalyser;
+    private ParticleSystem particleSystem;
+
+    // Use this for initialization
+    void Start () {
+        AudioSource audioSource = GetComponent<AudioSource>();
+        audioSpectrumAnalyser = new AudioSpectrumAnalyser(audioSource, AudioSpectrumAnalyser.AudioSampleSize.Medium, FFTWindow.Rectangular);
+
+        particleSystem = GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        audioSpectrumAnalyser.UpdateSpectrumData();
+
+        float[] audioSpectrumData = audioSpectrumAnalyser.audioSpectrumData;
+
+        ParticleSystem.MainModule main = particleSystem.main;
+        main.startSizeMultiplier = audioSpectrumData[audioSpectrumAnalyser.dominantFrequencyBandIndex]*2.0f; 
+    }
+}
